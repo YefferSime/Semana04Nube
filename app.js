0b9881e2d8e832e4732f704b683479506e56ba55
YefferSime <simehuaranccay@gmail.com>
2024-04-04 11:55:02 -0500

    Yeffer

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/productos', (req, res) => {
   res.send('Lista de productos: producto1, producto2, producto3');
 });
 
-const PORT = process.env.PORT || 12700;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+
+app.listen(12700, () => {
+  console.log(`Servidor corriendo en el puerto 5000`);
 });
